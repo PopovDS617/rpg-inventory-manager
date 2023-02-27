8e62338a5c58f98f909bd9ed60051fbb2682bebd
PopovDS617 <popovds.bm@gmail.com>
2023-02-27 23:42:14 +0300

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+ 
+import { Inter } from 'next/font/google'
+ 
+
+ 
+
+export default function Home() {
+  return (
+    <main >
+      <div className='text-5xl'>
+     hellp there
+      </div>
+    </main>
+  )
+}
