7f4c0ff43df3124e0339b63820ee7f495f99b3b0
PopovDS617 <popovds.bm@gmail.com>
2023-03-01 23:17:15 +0300

    feat: add basic components

---
+++ src/components/Items/ItemList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ItemList = (props: Props) => {
+  return <div>ItemList</div>;
+};
+
+export default ItemList;
---
+++ src/components/Items/SingleItem.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SingleItem = (props: Props) => {
+  return <div>SingleItem</div>;
+};
+
+export default SingleItem;
